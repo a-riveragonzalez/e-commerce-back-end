f4c172d69497e3cc90b284cc644f6f097acbf90e
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-09-28 21:51:42 -0700

    added delete routes to tag, product, and category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,7 +37,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // todo create a new category
+  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -50,8 +50,24 @@ router.put('/:id', (req, res) => {
   // todo update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // todo delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  // deletes a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
