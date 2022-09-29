6535bb24e55132740e0f816c1df603ea21dea074
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-09-28 21:35:52 -0700

    added the create tag and create category route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,8 +36,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // todo create a new tag
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
